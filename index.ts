99e64b35d77df573838ba1181ff23f14b5fbbf12
zheeeng <hi@zheeeng.me>
2022-02-15 17:06:26 +0800

    fix: break changes for support resoving file from parents

--- index.ts
+++ index.ts
@@ -13,13 +13,17 @@ export default function plainText (virtualFileId = defaultVirtualFileId) {
   return {
     name: 'virtual-plain-text', // required, will show up in warnings and errors
     resolveId(id: string) {
-      if (id.indexOf(virtualFileId) === 0) {
-        return id
+      const indexOfVirtualFileId = id.indexOf(virtualFileId)
+      if (indexOfVirtualFileId === 0) {
+        return `${virtualFileId}${encodeURIComponent(id.slice(virtualFileId.length))}`
       }
     },
     async load(id: string) {
-      if (id.indexOf(virtualFileId) === 0) {
-        const filePath = path.resolve(id.replace(virtualFileId, ''))
+      const indexOfVirtualFileId = id.indexOf(virtualFileId)
+
+      if (indexOfVirtualFileId === 0) {
+        const loadId = decodeURIComponent(id.replace(virtualFileId, ''))
+        const filePath = path.resolve(loadId)
         const content = await fs.promises.readFile(filePath, { encoding: 'utf-8' })
         return `export const plainText = ${JSON.stringify(content)}`
       }
