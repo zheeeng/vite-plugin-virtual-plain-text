ad24cbc069df39893b2bcc5a04ebdf23d3097da0
zheeeng <hi@zheeeng.me>
2022-02-15 17:09:38 +0800

    refactor: change implementation

--- index.ts
+++ index.ts
@@ -13,16 +13,13 @@ export default function plainText (virtualFileId = defaultVirtualFileId) {
   return {
     name: 'virtual-plain-text', // required, will show up in warnings and errors
     resolveId(id: string) {
-      const indexOfVirtualFileId = id.indexOf(virtualFileId)
-      if (indexOfVirtualFileId === 0) {
+      if (id.indexOf(virtualFileId) === 0) {
         return `${virtualFileId}${encodeURIComponent(id.slice(virtualFileId.length))}`
       }
     },
     async load(id: string) {
-      const indexOfVirtualFileId = id.indexOf(virtualFileId)
-
-      if (indexOfVirtualFileId === 0) {
-        const loadId = decodeURIComponent(id.replace(virtualFileId, ''))
+      if (id.indexOf(virtualFileId)) {
+        const loadId = decodeURIComponent(id.slice(virtualFileId.length))
         const filePath = path.resolve(loadId)
         const content = await fs.promises.readFile(filePath, { encoding: 'utf-8' })
         return `export const plainText = ${JSON.stringify(content)}`
