e1a3d10dc62fc614976510df0fdc7db1fdb75125
慕还 <zhengjiang.jz@antgroup.com>
2023-11-07 17:22:42 +0800

    chore: bump build scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: false,
   clean: true,
   dts: true,
-  format: ['cjs', 'esm']
+  format: ['esm', 'cjs']
 })
